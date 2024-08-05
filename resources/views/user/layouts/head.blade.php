1c4657b37a2507f2cc8af2c3b8e9f32a3184761c
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2024-08-05 10:47:10 +0630

    update a

--- resources/views/user/layouts/head.blade.php
+++ resources/views/user/layouts/head.blade.php
@@ -4,7 +4,7 @@
 <head>
  <meta charset="UTF-8" />
  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
- <title>SuperBet Slot</title>
+ <title>Panda 666 Slot</title>
  <link rel="icon" href="{{ asset('/assets/img/logo.png') }}">
  <link rel="stylesheet" href="{{ asset('slot_app/css/style.css') }}" />
  <!-- Bootstrap 5 CSS -->
